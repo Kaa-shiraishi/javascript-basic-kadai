7f5f627fc2d8be9c553485b577a8d2b9b1aa3efb
Kaa-shiraishi <kaaaana19891113@gmail.com>
2025-04-19 06:56:00 +0900

    Delete kadai_014 directory

--- kadai_014/kadai_014/square.js
+++
@@ -1,6 +0,0 @@
-const squared  = (num) => {
-    return num * num;
-  }
-  
-  // 関数の戻り値を出力する
-  console.log(squared(10));
--- kadai_014/square.js
+++
@@ -1,6 +0,0 @@
-const squared  = (num) => {
-    return num * 10;
-  }
-  
-  // 関数の戻り値を出力する
-  console.log(squared(10));
